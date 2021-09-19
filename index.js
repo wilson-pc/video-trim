e96c35f1cbf137bd085dc70d7a78b761eec7fe31
wilson-pc <wilverhidalgo@outlook.com>
2021-09-19 13:16:17 -0400

    tiodo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require("./app");
-const port = 3007;
+const app = require('./app');
+const port = process.env.PORT | 3007;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
