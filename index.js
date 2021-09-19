d0ee489e0f0a8d1fa9f5cdd61bb48f05b699ba42
wilson-pc <wilverhidalgo@outlook.com>
2021-09-19 13:22:55 -0400

    port+

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT | 3007;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
